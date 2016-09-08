9c9fc6c292badf4d28e79acdf933acd3443e42fd
dyar74 <d_yar@mail.ru>
2016-09-08 06:28:10 -0700

    add migrations to portfolio

---
+++ console/migrations/m160908_131459_create_portfolio_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `portfolio`.
+ */
+class m160908_131459_create_portfolio_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('portfolio', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(100)->notNull(),
+            'image' => $this->string(100)->notNull(),
+            'display_order' => $this->integer(11)->notNull(),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
+            'status' => $this->integer()->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('portfolio');
+    }
+}
